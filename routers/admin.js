0ae42c9584b51dd1e0a0266024ba9f58574396cd
nfl404 <785558516@qq.com>
2017-11-07 17:32:44 +0800

    用户列表分页

--- routers/admin.js
+++ routers/admin.js
@@ -31,13 +31,40 @@ router.get('/',function (req,res,next) {
 router.get('/user',function (req, res, next) {
     /**
      * 从数据库中读取所有用户数据
+     *
+     * limit(Number):限制获取的数据条数
+     *
+     * skip(2):或略数据的条数
+     *
+     * 每页显示2条
+     * 1： 1-2 skip：0 -> (当前页-1)*limit
+     * 2： 3-4 skip：2
+     *
      */
-    User.find().then(function (users) {
-        //console.log(users);
-        res.render('admin/user', {
-            userInfo: req.userInfo,
-            users: users
-        })
+
+    var page = Number(req.query.page || 1);
+    var limit = 2;
+    var pages = 1;
+
+    User.count().then(function (count) {
+        //计算总页数
+        pages = Math.ceil(count/limit); //向上区取整
+        page = Math.min(page,pages); //取值不能超过pages
+        page = Math.max(page, 1); //取值不能小于1
+
+        var skip = (page -1 ) * 2;
+
+        User.find().limit(limit).skip(skip).then(function (users) {
+            //console.log(users);
+            res.render('admin/user', {
+                userInfo: req.userInfo,
+                users: users,
+                count: count,
+                pages: pages,
+                limit: limit,
+                page: page
+            })
+        });
     });
 
 });
